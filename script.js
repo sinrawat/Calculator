d7ef0c810ee73c0030edf29f04109a04d0a1acc5
film1023 <filmgg.com@gmail.com>
2026-02-05 11:23:33 +0700

    js add appendNumber&Operator

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let expression = "";
+
+function appendNumber(number) {
+    expression += number;
+    updateDisplay();
+}
+
+function appendOperator(operator) {
+    if (expression === "") return;
+    const lastChar = expression.slice(-1);
+    if ("+-*/%".includes(lastChar)) return;
+    expression += operator;
+    updateDisplay();
+}
 
